ac2f7014b00f63176a3fa848f90f8faef6a8eb1e
Alex Johnson <alexiionescu@gmail.com>
2025-08-29 09:20:34 +0000

    import residents from csv, fmt commit hook

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::fmt;
 
+use anyhow::Result;
+use clap::{Parser, Subcommand, arg, command};
+use csv::ReaderBuilder;
 use futures::TryStreamExt as _;
 use mongodb::{
-    bson::{self, doc}, error::{WriteError, WriteFailure}, options::{ClientOptions, IndexOptions, ServerApi, ServerApiVersion}, Client, Collection, IndexModel
+    Client, Collection, IndexModel,
+    bson::{self, doc},
+    error::{WriteError, WriteFailure},
+    options::{ClientOptions, IndexOptions, ServerApi, ServerApiVersion},
 };
-use tracing::{error, info, warn, Level};
-use anyhow::Result;
-use clap::{arg, command, Subcommand, Parser};
+use tracing::{Level, error, info, warn};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -20,6 +24,9 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum CliCommand {
+    InsertCsv {
+        file_path: String,
+    },
     Insert {
         name: String,
         birth: String,
@@ -43,9 +50,13 @@ enum CliCommand {
     Query {
         from_date: String,
         to_date: String,
-        #[clap(short,long,help = "Optional Regexp pattern to match resident names")]
+        #[clap(short, long, help = "Optional Regexp pattern to match resident names")]
         name: Option<String>,
-        #[clap(short,long,help = "Optional Regexp pattern to match resident locations")]
+        #[clap(
+            short,
+            long,
+            help = "Optional Regexp pattern to match resident locations"
+        )]
         location: Option<String>,
     },
     SimpleTest,
@@ -67,10 +78,14 @@ struct ActiveAlarm {
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Resident {
     name: String,
+    #[serde(with = "serde_helpers::bson_datetime_as_rfc3339_string_date")]
     birth: bson::DateTime,
     location: String,
+    #[serde(with = "serde_helpers::bson_datetime_as_rfc3339_string_date")]
     resident_since: bson::DateTime,
+    #[serde(default)]
     alarms: Vec<Alarm>,
+    #[serde(default)]
     active_alarms: Vec<ActiveAlarm>,
 }
 
@@ -80,7 +95,9 @@ impl Resident {
             name: name.to_string(),
             birth: bson::DateTime::parse_rfc3339_str(birth.to_string() + "T00:00:00Z")?,
             location: location.to_string(),
-            resident_since: bson::DateTime::parse_rfc3339_str(resident_since.to_string() + "T00:00:00Z")?,
+            resident_since: bson::DateTime::parse_rfc3339_str(
+                resident_since.to_string() + "T00:00:00Z",
+            )?,
             alarms: Vec::new(),
             active_alarms: Vec::new(),
         })
@@ -105,23 +122,23 @@ impl Resident {
 
 impl fmt::Display for Resident {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Resident {{ name: {}, birth: {}, location: {}, resident_since: {} }}",
-            self.name,
-            self.birth,
-            self.location,
-            self.resident_since,
+        write!(
+            f,
+            "Resident {{ name: {}, birth: {}, location: {}, resident_since: {} }}",
+            self.name, self.birth, self.location, self.resident_since,
         )?;
         for active_alarm in &self.active_alarms {
-            write!(f, "\n  ActiveAlarm {{ time: {}, message: {} }}",
-                active_alarm.time,
-                active_alarm.message,
+            write!(
+                f,
+                "\n  ActiveAlarm {{ time: {}, message: {} }}",
+                active_alarm.time, active_alarm.message,
             )?;
         }
         for alarm in &self.alarms {
-            write!(f, "\n  HistoryAlarm {{ time: {}, duration_sec: {}, message: {} }}",
-                alarm.time,
-                alarm.duration_sec,
-                alarm.message,
+            write!(
+                f,
+                "\n  HistoryAlarm {{ time: {}, duration_sec: {}, message: {} }}",
+                alarm.time, alarm.duration_sec, alarm.message,
             )?;
         }
         Ok(())
@@ -134,8 +151,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     dotenv::dotenv().ok();
     let mongodb_uri = dotenv::var("MONGODB_URI").expect("MONGODB_URI must be set in .env");
-    let mut client_options =
-    ClientOptions::parse(mongodb_uri).await?;
+    let mut client_options = ClientOptions::parse(mongodb_uri).await?;
 
     // Set the server_api field of the client_options object to set the version of the Stable API on the client
     let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
@@ -158,7 +174,12 @@ async fn main() -> Result<()> {
     collection.create_index(unique_index).await?;
 
     match &cli.command {
-        CliCommand::Insert { name, birth, location, resident_since } => {
+        CliCommand::Insert {
+            name,
+            birth,
+            location,
+            resident_since,
+        } => {
             let resident = Resident::new(name, birth, location, resident_since)?;
             if cli.upsert {
                 test_upsert(&collection, resident).await?;
@@ -172,23 +193,74 @@ async fn main() -> Result<()> {
         CliCommand::SimpleTest => {
             simple_test(&collection).await?;
         }
-        CliCommand::NewAlarm { name, birth, message } => {
+        CliCommand::NewAlarm {
+            name,
+            birth,
+            message,
+        } => {
             test_new_alarm(&collection, name, birth, message).await?;
         }
-        CliCommand::ClearAlarm { name, birth, alarm_time } => {
+        CliCommand::ClearAlarm {
+            name,
+            birth,
+            alarm_time,
+        } => {
             test_clear_alarm(&collection, name, birth, alarm_time).await?;
         }
-        CliCommand::Query { from_date, to_date, name, location } => {
-            test_query(&collection, from_date, to_date, name.as_deref(), location.as_deref()).await?;
+        CliCommand::Query {
+            from_date,
+            to_date,
+            name,
+            location,
+        } => {
+            test_query(
+                &collection,
+                from_date,
+                to_date,
+                name.as_deref(),
+                location.as_deref(),
+            )
+            .await?;
+        }
+        CliCommand::InsertCsv { file_path } => {
+            test_insert_csv(&collection, file_path, cli.upsert).await?;
         }
     }
 
     Ok(())
 }
 
+async fn test_insert_csv(
+    collection: &Collection<Resident>,
+    file_path: &str,
+    upsert: bool,
+) -> Result<()> {
+    let mut reader = ReaderBuilder::new()
+        .has_headers(true)
+        .from_path(file_path)?;
+    for result in reader.deserialize() {
+        let record: Resident = result?;
+        println!("Importing {}", record);
+        if upsert {
+            test_upsert(collection, record).await?;
+        } else {
+            test_insert_or_update(collection, record).await?;
+        }
+    }
+    Ok(())
+}
+
 #[tracing::instrument(name = "query", skip(collection), level = Level::TRACE)]
-async fn test_query(collection: &Collection<Resident>, from_date: &str, to_date: &str, name: Option<&str>, location: Option<&str>) -> Result<()> {
-    let mut filter = if let Some(name_pattern) = name && let Some(location_pattern) = location {
+async fn test_query(
+    collection: &Collection<Resident>,
+    from_date: &str,
+    to_date: &str,
+    name: Option<&str>,
+    location: Option<&str>,
+) -> Result<()> {
+    let mut filter = if let Some(name_pattern) = name
+        && let Some(location_pattern) = location
+    {
         doc! {
             "$or": [
                 { "name": { "$regex": name_pattern, "$options": "i" } },
@@ -210,12 +282,12 @@ async fn test_query(collection: &Collection<Resident>, from_date: &str, to_date:
     });
     let pipeline = vec![
         doc! { "$match": filter },
-        doc! { "$project": { 
-            "name": 1, "location": 1, 
-            "alarms_count": { "$size": "$alarms" }, 
+        doc! { "$project": {
+            "name": 1, "location": 1,
+            "alarms_count": { "$size": "$alarms" },
             "alarms_avg_duration": { "$avg": "$alarms.duration_sec" },
             "active_alarms_count": { "$size": { "$ifNull": ["$active_alarms", []] } }
-        } }
+        } },
     ];
     match collection.aggregate(pipeline).await {
         Ok(mut cursor) => {
@@ -231,7 +303,12 @@ async fn test_query(collection: &Collection<Resident>, from_date: &str, to_date:
 }
 
 #[tracing::instrument(name = "new_alarm", skip_all, fields(name=%name, birth=%birth), level = Level::TRACE)]
-async fn test_new_alarm(collection: &Collection<Resident>, name: &str, birth: &str, message: &str) -> Result<()> {
+async fn test_new_alarm(
+    collection: &Collection<Resident>,
+    name: &str,
+    birth: &str,
+    message: &str,
+) -> Result<()> {
     let birth_date = bson::DateTime::parse_rfc3339_str(birth.to_string() + "T00:00:00Z")?;
     let filter = doc! {
         "name": name,
@@ -247,7 +324,12 @@ async fn test_new_alarm(collection: &Collection<Resident>, name: &str, birth: &s
     match collection.update_one(filter, update).await {
         Ok(update_result) => {
             if update_result.matched_count > 0 {
-                info!("Alarm {} added to resident. Matched: {} Updated: {}", new_alarm.time.try_to_rfc3339_string()?, update_result.matched_count, update_result.modified_count);
+                info!(
+                    "Alarm {} added to resident. Matched: {} Updated: {}",
+                    new_alarm.time.try_to_rfc3339_string()?,
+                    update_result.matched_count,
+                    update_result.modified_count
+                );
             } else {
                 warn!("No resident found to add alarm.");
             }
@@ -259,7 +341,12 @@ async fn test_new_alarm(collection: &Collection<Resident>, name: &str, birth: &s
     Ok(())
 }
 #[tracing::instrument(name = "clear_alarm", skip_all, fields(name=%name, birth=%birth), level = Level::TRACE)]
-async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: &str, alarm_time: &str) -> Result<()> {
+async fn test_clear_alarm(
+    collection: &Collection<Resident>,
+    name: &str,
+    birth: &str,
+    alarm_time: &str,
+) -> Result<()> {
     let birth_date = bson::DateTime::parse_rfc3339_str(birth.to_string() + "T00:00:00Z")?;
     let start_time = bson::DateTime::parse_rfc3339_str(alarm_time)?;
     let filter = doc! {
@@ -281,8 +368,17 @@ async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: 
         let alarm_doc = alarm_array[0].as_document().unwrap();
         let message = alarm_doc.get_str("message").unwrap_or("");
         let alarm_time = alarm_doc.get_datetime("time").unwrap();
-        let duration = bson::DateTime::now().checked_duration_since(*alarm_time).unwrap_or_default().as_secs();
-        info!("Clearing alarm for resident id: {:?}, message: {}, start_time: {}, duration_sec: {}", resident_id, message, alarm_time.try_to_rfc3339_string()?, duration);
+        let duration = bson::DateTime::now()
+            .checked_duration_since(*alarm_time)
+            .unwrap_or_default()
+            .as_secs();
+        info!(
+            "Clearing alarm for resident id: {:?}, message: {}, start_time: {}, duration_sec: {}",
+            resident_id,
+            message,
+            alarm_time.try_to_rfc3339_string()?,
+            duration
+        );
 
         // remove alarm from active
         let filter = doc! {
@@ -298,7 +394,10 @@ async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: 
         match collection.update_one(filter.clone(), update).await {
             Ok(update_result) => {
                 if update_result.matched_count > 0 {
-                    info!("Alarm cleared from active for resident. Matched: {} Updated: {}", update_result.matched_count, update_result.modified_count);
+                    info!(
+                        "Alarm cleared from active for resident. Matched: {} Updated: {}",
+                        update_result.matched_count, update_result.modified_count
+                    );
                 } else {
                     warn!("No resident found to clear alarm.");
                 }
@@ -321,7 +420,10 @@ async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: 
         match collection.update_one(filter, history_update).await {
             Ok(update_result) => {
                 if update_result.matched_count > 0 {
-                    info!("Alarm added to history for resident. Matched: {} Updated: {}", update_result.matched_count, update_result.modified_count);
+                    info!(
+                        "Alarm added to history for resident. Matched: {} Updated: {}",
+                        update_result.matched_count, update_result.modified_count
+                    );
                 } else {
                     warn!("No resident found to add alarm to history.");
                 }
@@ -356,7 +458,7 @@ async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: 
     //     }
     // }
     // let update = doc! {
-    //     "$set": { 
+    //     "$set": {
     //         "active_alarms" : {
     //             "$filter": {
     //                 "input": "$active_alarms",
@@ -367,7 +469,7 @@ async fn test_clear_alarm(collection: &Collection<Resident>, name: &str, birth: 
     //         "alarms" : {
     //             "$concatArrays": [
     //                 "$alarms",
-    //                 { 
+    //                 {
     //                     "$filter": {
     //                         "input": "$active_alarms",
     //                         "as": "alarm",
@@ -420,11 +522,20 @@ async fn test_delete(collection: &Collection<Resident>, name: &str, birth: &str)
 async fn test_upsert(collection: &Collection<Resident>, resident: Resident) -> Result<()> {
     let filter = resident.unique_index();
     let update = resident.update_data();
-    let options = mongodb::options::UpdateOptions::builder().upsert(true).build();
-    match collection.update_one(filter, update).with_options(options).await {
+    let options = mongodb::options::UpdateOptions::builder()
+        .upsert(true)
+        .build();
+    match collection
+        .update_one(filter, update)
+        .with_options(options)
+        .await
+    {
         Ok(update_result) => {
             if update_result.matched_count > 0 {
-                info!("Resident updated Matched: {} Updated: {}", update_result.matched_count, update_result.modified_count);
+                info!(
+                    "Resident updated Matched: {} Updated: {}",
+                    update_result.matched_count, update_result.modified_count
+                );
             } else if let Some(upserted_id) = update_result.upserted_id {
                 info!("New resident inserted with id: {}", upserted_id);
             }
@@ -437,34 +548,41 @@ async fn test_upsert(collection: &Collection<Resident>, resident: Resident) -> R
 }
 
 #[tracing::instrument(name = "insert_or_update", skip_all, fields(name=%resident.name, birth=%resident.birth), level = Level::TRACE)]
-async fn test_insert_or_update(collection: &Collection<Resident>, resident: Resident) -> Result<()> {
+async fn test_insert_or_update(
+    collection: &Collection<Resident>,
+    resident: Resident,
+) -> Result<()> {
     match collection.insert_one(&resident).await {
         Ok(insert_result) => {
-            info!("New resident inserted with id: {}", insert_result.inserted_id);
+            info!(
+                "New resident inserted with id: {}",
+                insert_result.inserted_id
+            );
         }
-        Err(e) => {
-            match e.kind.as_ref() {
-                mongodb::error::ErrorKind::Write(write_failure) => {
-                    match write_failure {
-                        WriteFailure::WriteError(WriteError {code: 11000, ..}) => {
-                            warn!("Duplicate key error: A resident with the same name and birth date already exists. Updating...");
-                            let filter = resident.unique_index();
-                            let update = resident.update_data();
-                            match collection.update_one(filter, update).await {
-                                Ok(update_result) => {
-                                    info!("Resident updated Matched: {} Updated: {}", update_result.matched_count, update_result.modified_count);
-                                }
-                                Err(e) => {
-                                    error!("Failed to update resident: {}", e);
-                                }
-                            }
+        Err(e) => match e.kind.as_ref() {
+            mongodb::error::ErrorKind::Write(write_failure) => match write_failure {
+                WriteFailure::WriteError(WriteError { code: 11000, .. }) => {
+                    warn!(
+                        "Duplicate key error: A resident with the same name and birth date already exists. Updating..."
+                    );
+                    let filter = resident.unique_index();
+                    let update = resident.update_data();
+                    match collection.update_one(filter, update).await {
+                        Ok(update_result) => {
+                            info!(
+                                "Resident updated Matched: {} Updated: {}",
+                                update_result.matched_count, update_result.modified_count
+                            );
                         }
-                        _ => error!("Failed to insert new resident: {}", e),
+                        Err(e) => {
+                            error!("Failed to update resident: {}", e);
+                        }
                     }
                 }
                 _ => error!("Failed to insert new resident: {}", e),
-            }
-        }
+            },
+            _ => error!("Failed to insert new resident: {}", e),
+        },
     }
     Ok(())
 }
@@ -482,4 +600,42 @@ async fn simple_test(collection: &Collection<Resident>) -> Result<()> {
     test_delete(collection, "John Doe", "1990-01-01").await?;
     test_delete(collection, "Jane Smith", "1985-05-15").await?;
     Ok(())
+}
+
+mod serde_helpers {
+    pub mod bson_datetime_as_rfc3339_string_date {
+        use mongodb::bson;
+        use serde::{Deserialize, Deserializer, Serializer, de, ser};
+        use std::result::Result;
+
+        /// Deserializes a [`bson::DateTime`] from an RFC 3339 formatted string.
+        pub fn deserialize<'de, D>(deserializer: D) -> Result<bson::DateTime, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let mut iso = String::deserialize(deserializer)?;
+            if !iso.contains('T') {
+                // assume date only
+                iso.push_str("T00:00:00Z");
+            } else if !iso.ends_with('Z') && !iso.contains('+') && !iso.contains('-') {
+                // assume UTC if no timezone provided
+                iso.push('Z');
+            }
+            let date = bson::DateTime::parse_rfc3339_str(&iso).map_err(|_| {
+                de::Error::custom(format!("cannot parse RFC 3339 datetime from \"{}\"", iso))
+            })?;
+            Ok(date)
+        }
+
+        /// Serializes a [`bson::DateTime`] as an RFC 3339 (ISO 8601) formatted string.
+        pub fn serialize<S: Serializer>(
+            val: &bson::DateTime,
+            serializer: S,
+        ) -> Result<S::Ok, S::Error> {
+            let formatted = val.try_to_rfc3339_string().map_err(|e| {
+                ser::Error::custom(format!("cannot format {} as RFC 3339: {}", val, e))
+            })?;
+            serializer.serialize_str(&formatted)
+        }
+    }
 }
